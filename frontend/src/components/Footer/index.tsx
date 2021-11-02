1bcd9730a3d6e67eed059a9cbe8d226afec1e964
Julio Cesar <julio.cq96@gmail.com>
2021-11-02 16:14:21 -0300

    Test ci/cd

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
     <footer className="footer mt-auto py-3 bg-dark">
         <div className="container">
-            <p className="text-light">App developed by <a href="https://github.com/juuliocq" target="_blank" rel="noreferrer">Julio Cesar Queiroz</a></p>
+            <p className="text-light">App developed by <a href="https://github.com/juuliocq" target="_blank" rel="noreferrer">Cesar Queiroz</a></p>
             <p className="text-light"><small><strong>Spring React Week</strong><br/>
             Event promoted by the DevSuperior school: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
         </div>
