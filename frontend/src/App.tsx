adc8a363c147d76dbe2eac768094823968cafb36
Julio Cesar <julio.cq96@gmail.com>
2021-11-02 15:34:18 -0300

    add First Static Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import NavBar from "components/navbar";
+import Footer from "components/footer"
+import DataTable from "components/datatable"
 
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá mundo!</h1>
+      <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
